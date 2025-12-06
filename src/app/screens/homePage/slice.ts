11bd12fb7ba8c18a1540a3e959a1730e8ad631c2
Oybek <oybeksobitovxyz@gmail.com>
2025-12-06 14:11:15 +0900

    feat: create homePage Redux configuration

---
+++ src/app/screens/homePage/slice.ts
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { HomePageState } from "../../../lib/types/screen";
+import exp from "constants";
+
+const initialState: HomePageState = {
+    popularDishes: [],
+    newDishes: [],
+    topUsers: [],
+};
+
+const HomePageSlice = createSlice({
+    name: "homePage",
+    initialState,
+    reducers: {
+        setPopularDishes: (state, action) => {
+            state.popularDishes = action.payload;
+        },
+        setNewDishes: (state, action) => {
+            state.newDishes = action.payload;
+        },
+        setTopUsers: (state, action) => {
+            state.topUsers = action.payload;
+        },
+    },
+ });
+
+ export const {setPopularDishes, setNewDishes, setTopUsers} = HomePageSlice.actions;
+
+ const HomePageReducer = HomePageSlice.reducer;
+ export default HomePageReducer;
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,10 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import HomePage from "./screens/homePage";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    HomePage
+  },
 });
 
 export type AppDispatch = typeof store.dispatch;
