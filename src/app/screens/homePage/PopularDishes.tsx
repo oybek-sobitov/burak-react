0255432dd90f29b84ab82d3e00cf299dfced2a1f
Oybek <oybeksobitovxyz@gmail.com>
2025-12-07 23:53:17 +0900

    fix: retreive fresh menu and modify new dishes component

--- src/app/screens/homePage/PopularDishes.tsx
+++ src/app/screens/homePage/PopularDishes.tsx
@@ -22,8 +22,6 @@ const popularDishesRetriever = createSelector(
 
 export default function PopularDishes() {
   const {popularDishes} = useSelector(popularDishesRetriever);
-
-  console.log("popularDishes:", popularDishes);
     return (
     <div className="popular-dishes-frame">
         <Container>
@@ -31,10 +29,10 @@ export default function PopularDishes() {
                 <Box className="category-title">Popular Dishes</Box>
                 <Stack className="cards-frame">
                     {popularDishes.length !==0 ? (
-                    popularDishes.map((ele: Product) => {
-                        const imagePath = `${serverAPi}/${ele.productImages[0]}`
+                    popularDishes.map((product: Product) => {
+                        const imagePath = `${serverAPi}/${product.productImages[0]}`
                         return (
-                            <CssVarsProvider key={ele._id}>
+                            <CssVarsProvider key={product._id}>
                                 <Card className={"card"}>
                                     <CardCover>
                                         <img
@@ -49,7 +47,7 @@ export default function PopularDishes() {
                                                 textColor="#fff"
                                                 mb={1}
                                             >
-                                                {ele.productName}
+                                                {product.productName}
                                             </Typography>
                                             <Typography
                                             sx={{
@@ -59,7 +57,7 @@ export default function PopularDishes() {
                                                 display: "flex",
                                             }}
                                             >
-                                                {ele.productViews}
+                                                {product.productViews}
                                                 <VisibilityIcon
                                                     sx={{ fontsize: 25, marginLeft: "5px"}}
                                                 />
@@ -80,7 +78,7 @@ export default function PopularDishes() {
                                             startDecorator={<DescriptionOutlinedIcon />}
                                             textColor="neutral.300"
                                         >
-                                            {ele.productDesc}
+                                            {product.productDesc}
                                         </Typography>
                                     </CardOverflow>
                                 </Card>
